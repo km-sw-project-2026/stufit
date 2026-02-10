5e9c23b9af7711d4a2cae804a5460753b76633ec
권민서 <qbuild07@gmail.com>
2026-02-10 14:59:59 +0900

    feat: 포인트 로그 테이블에 points 컬럼 추가 및 관련 SQL 스크립트 생성

---
+++ scripts/cleanup_selected_users_nontx.sql
@@ -0,0 +1,15 @@
+-- Non-transactional deletion (child tables first, then users)
+DELETE FROM challenge_members WHERE user_id IN (7,8,9,10,11);
+DELETE FROM challenge_daily_progress WHERE user_id IN (7,8,9,10,11);
+DELETE FROM challenge_results WHERE user_id IN (7,8,9,10,11);
+DELETE FROM point_logs WHERE user_id IN (7,8,9,10,11);
+DELETE FROM attendance_logs WHERE user_id IN (7,8,9,10,11);
+DELETE FROM user_profiles WHERE user_id IN (7,8,9,10,11);
+DELETE FROM user_items WHERE user_id IN (7,8,9,10,11);
+DELETE FROM item_wishlist WHERE user_id IN (7,8,9,10,11);
+DELETE FROM megaphone_logs WHERE user_id IN (7,8,9,10,11);
+DELETE FROM post_likes WHERE user_id IN (7,8,9,10,11);
+DELETE FROM comment_likes WHERE user_id IN (7,8,9,10,11);
+DELETE FROM comments WHERE user_id IN (7,8,9,10,11);
+DELETE FROM posts WHERE user_id IN (7,8,9,10,11);
+DELETE FROM users WHERE user_id IN (7,8,9,10,11);
