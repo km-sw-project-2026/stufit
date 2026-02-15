b86969a61a7fd369149bc2c27e51724e5b56c40f
Hyunseo Park <phsgp0701@gmail.com>
2026-02-15 21:47:50 +0900

    보상 시스템 개선: 점수 및 포인트 계산 로직 수정 및 사용자 안내 메시지 추가

--- src/components/modals/ChallengeOverModal.jsx
+++ src/components/modals/ChallengeOverModal.jsx
@@ -59,7 +59,7 @@ function ChallengeOverModal({
             <div className="ranking-list">
               {rankingData.length === 0 ? (
                 <div className="ranking-item">
-                  <span className="name">랭킹 데이터가 없습니다.</span>
+                  <span className="name">혼자하는 챘린지는 점수와 포인트 제공이 제한 됩니다! (악용방지)</span>
                 </div>
               ) : (
                 rankingData.map((item) => {
