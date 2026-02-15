42d424fcde7cecd57e416beb5673dcbefe25bff3
kimyesun <kimyesun1116@gmail.com>
2026-02-15 22:22:15 +0900

    ShopQuicklink 컴포넌트: 배지 레이블을 동적으로 업데이트

--- src/components/main/ShopQuicklink.jsx
+++ src/components/main/ShopQuicklink.jsx
@@ -35,6 +35,23 @@ function ShopQuicklink() {
     pumpkin,
   ];
 
+  // Badge labels per image (localized)
+  const badges = [
+    '치아 배경',
+    '치킨 액자',
+    'VIP 액자',
+    '초록 해파리',
+    '레몬 배경',
+    '아보카도 배경',
+    '벚꽃 액자',
+    '레몬 액자',
+    '고스트',
+    '앵무새',
+    '오리',
+    '핑크 도트',
+    '호박',
+  ];
+
   const n = images.length;
 
   // State: which logical index is centered
@@ -233,7 +250,7 @@ function ShopQuicklink() {
         </div>
 
         <div className="shop-item-info" style={{ textAlign: 'center', marginTop: 16 }}>
-          <div className="shop-badge">프로필 액자</div>
+          <div className="shop-badge">{badges[centerIndex] || '아이템'}</div>
           <div className="shop-name">아이템 미리보기</div>
         </div>
 
