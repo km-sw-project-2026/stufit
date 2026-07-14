d368ea1eed53df193909dbda6ba3878510676d8f
kimyesun <kimyesun1116@gmail.com>
2026-07-15 01:42:52 +0900

    fix: inline social-config in worker to avoid import error

--- worker/index.js
+++ worker/index.js
@@ -8,7 +8,6 @@ import { verifyToken } from "../functions/api/utils/jwt";
 import * as login from "../functions/api/auth/login";
 import * as register from "../functions/api/auth/register";
 import * as logout from "../functions/api/auth/logout";
-import * as socialConfig from "../functions/api/auth/social-config";
 import * as naverCallback from "../functions/api/auth/naver/callback";
 import * as kakaoCallback from "../functions/api/auth/kakao/callback";
 
@@ -174,7 +173,17 @@ export default {
       }
 
       if (pathname === "/api/auth/social-config") {
-        return socialConfig.onRequestGet({ request, env });
+        const url = new URL(request.url);
+        const origin = url.origin;
+        const naverClientId = env.NAVER_CLIENT_ID || "";
+        const kakaoClientId = env.KAKAO_CLIENT_ID || "";
+        const naverState = Math.random().toString(36).substring(2, 10);
+        const naverRedirect = `${origin}/api/auth/naver/callback`;
+        const kakaoRedirect = `${origin}/api/auth/kakao/callback`;
+        return new Response(JSON.stringify({
+          naver: naverClientId ? `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${naverClientId}&redirect_uri=${encodeURIComponent(naverRedirect)}&state=${naverState}` : null,
+          kakao: kakaoClientId ? `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${kakaoClientId}&redirect_uri=${encodeURIComponent(kakaoRedirect)}` : null,
+        }), { status: 200, headers: { "Content-Type": "application/json" } });
       }
 
       if (pathname === "/api/auth/naver/callback") {
